eb779b1e443423ddeb2613cc4d428c8047760bef
Khaled_Ghaly <khaledghaly000@gmail.com>
2024-12-02 19:23:23 +0200

    defining user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define(
+    'user', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey:true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isVerified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+})
+
+
+module.exports = User;
