ebd21a2efcad6b2502b768c181bfda6a9c7e4615
Khaled_Ghaly <khaledghaly000@gmail.com>
2024-12-02 19:20:25 +0200

    database(feature): initial database using sequelize

---
+++ util/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('social_app', 'postgres', '123456',{
+  dialect: 'postgres',      
+  host: 'localhost',
+  port: 5432,
+  ssl: false,              
+  logging: console.log,     
+});
+
+sequelize.sync();
+
+module.exports = sequelize;  // Use ES modules export syntax
