a307a969928b45e5a6e74f0bd191e93755a237f6
Khaled_Ghaly <khaledghaly000@gmail.com>
2024-12-05 19:21:16 +0200

    rateLimiter for otp(feat): use express-rate-limiter for requesting otp

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,33 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/auth');
 const { body } = require('express-validator');
+const rateLimiter = require('express-rate-limit');
+const { StandardValidation } = require('express-validator');
 
+const otpRateLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message:{ message: 'Too many OTP requests. Please try again later.' },
+    StandardHeaders: true,
+    legacyHeaders: false
+})
 
 router.post('/signup', authController.signup)
 
-router.post('/verifyOtp', authController.verifyOtp)
+router.post('/verifyOtp', otpRateLimiter, authController.verifyOtp)
 
 router.post('/login', authController.login);
 
-router.post('/resetPassword', authController.resetPassword);
+router.post('/resetPassword', otpRateLimiter, authController.resetPassword);
 
-router.post('/verifyResetPass', authController.verifyResetPass);
+router.post('/verifyResetPass', otpRateLimiter, authController.verifyResetPass);
 
-router.post('/resetEmail', authController.resetEmail);
+router.post('/resetEmail', otpRateLimiter, authController.resetEmail);
 
-router.post('/verifyResetEmail', authController.verifyResetEmail);
+router.post('/verifyResetEmail', otpRateLimiter, authController.verifyResetEmail);
 
-router.post('/reqOtp', authController.reqOtp);//old version
+router.post('/reqOtp', otpRateLimiter, authController.reqOtp);//old version
+
 // router.get('/verify/:token', authController.verify);//old version
 
 module.exports = router;
